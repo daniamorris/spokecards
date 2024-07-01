fc045876e665908c6697566a4bdaac71622efc40
daniamorris <daniamorris@gmail.com>
2024-06-30 23:18:34 -0700

    first push

---
+++ web/app/buy-me-coffee/_contracts/useBuyMeACoffeeContract.ts
@@ -0,0 +1,16 @@
+import { baseSepolia } from 'viem/chains';
+import { generateContractHook } from '@/hooks/contracts';
+import BuyMeACoffeeABI from './BuyMeACoffeeABI';
+
+/**
+ * Returns contract data for the BuyMeACoffee contract.
+ */
+export const useBuyMeACoffeeContract = generateContractHook({
+  abi: BuyMeACoffeeABI,
+  [baseSepolia.id]: {
+    chain: baseSepolia,
+    address: '0x4A986798a3496DE38bD745eDD5f5C6c8183C575D',
+  },
+
+  // ... more chains for this contract go here
+});
